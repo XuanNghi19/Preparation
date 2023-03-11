6923a8d6c77ff3feccb095a73f5092ad519ade10
XuanNghi19 <XuanNghi19T@outlook.com>
2023-03-11 10:16:02 +0700

    try_sec

---
+++ HackerRank(c++)/data_type.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    int a;
+    double b;
+    string s1;
+    cin >> a >> b;
+    scanf("\n");
+    getline(cin, s1);
+    cout << a+i << endl;
+    cout << setprecision(1) << fixed << b+d << endl;
+    cout << s + s1;
+
+    return 0;
+}
