3ea8ddcaebf096f4deb07eee393b17c6dbd7401a
XuanNghi19 <XuanNghi19T@outlook.com>
2023-03-12 20:24:58 +0700

    Day 5

---
+++ HackerRank(30 Days of Code)/Day5_Loop.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(n_temp);
+    for(int i = 1; i<=10; i++)
+    {
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+    return 0;
+}
+
